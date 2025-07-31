b7d2006708752549acfcd1ec9cb114cf6e68f2c6
Dhruvbindal288 <dhruvbindal288@gmail.com>
2025-07-31 16:28:08 +0530

    Made User_Model

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,27 @@
+const mongoose=require('mongoose')
+
+const userSchema=new mongoose.Schema({
+    name:{type:String,required:true},
+    email:{type:String,required:true,unique:true},
+    password:{type:String,required:true},
+    profilPic:{type:String,default:""},
+    followers: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    }
+  ],
+
+  following: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    }
+  ],
+
+},{timestamps:true})
+
+const User = mongoose.model("Users", userSchema);
+
+
+export default User;
