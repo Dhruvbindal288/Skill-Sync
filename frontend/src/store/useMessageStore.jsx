c591e81ca20bcd970234077af833e578ff13d700
Dhruvbindal288 <dhruvbindal288@gmail.com>
2025-08-14 14:56:15 +0530

    Create message store in frontend

--- frontend/src/store/useMessageStore.jsx
+++ frontend/src/store/useMessageStore.jsx
@@ -3,11 +3,13 @@ import {create} from 'zustand'
 import { axiosInstance } from '../lib/axios_init'
 import { toast } from 'react-toastify';
 const useMessageStore=create((set)=>({
+    sidebarUsers: [],
     getUserForSideBar:async()=>{
         try {
             let res=await axiosInstance.get("/messages/sidebar-users")
+              set({ sidebarUsers: res.data });
         } catch (error) {
-            toast.error()
+            toast.error(error.response?.data?.message || "Server Error")
         }
     }
 }));
