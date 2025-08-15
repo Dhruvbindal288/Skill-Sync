668835c15218195cd1cd74fc5dc098f6f95258c1
Dhruvbindal288 <dhruvbindal288@gmail.com>
2025-08-15 12:44:55 +0530

    Changed the design for navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
-
+import logopng from '../assets/logo.png'
+import {NavLink} from 'react-router-dom'
 function Navbar() {
   return (
-    <div>
-      This is navbar
+    <div className='h-14 flex items-center justify-between p-6 bg-blue-50 shadow-inner'> 
+      <div className='h-13 w-14'><img src={logopng} alt="Skill-Sync"  className='w-full h-full'/></div>
+      <ul className='flex gap-16'>
+        <li className='hover:text-blue-400'><NavLink to={"/"}>Home</NavLink></li>
+            <li className='hover:text-blue-400'><NavLink to={"/aboutus"}>AboutUs</NavLink></li>
+                <li className='hover:text-blue-400'><NavLink to={"/browseskill"}>BrowseSills</NavLink></li>
+                 <li className='hover:text-blue-400'><NavLink to={"/chats"}>Chats</NavLink></li>
+<li className='hover:text-blue-400'>Profile</li>
+      </ul>
     </div>
   )
 }
